3b314c7e64d65bc8297e742edf5ec6b015dac1e0
elvin <elvin@gmail.com>
2022-11-25 19:04:36 +0800

    feat: added models and routes

---
+++ server/models/Project.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+    name: String,
+    description:  String,
+    thumbnail: String,
+    souceLink: Array,
+    dateCreated: { type: Date, default: Date.now },
+    dateUpdated: { type: Date, default: Date.now },
+    authorID: { 
+        type: mongoose.Schema.Types.ObjectId,n
+        ref: 'User'
+    },
+    badgeID: 
+        { type: mongoose.Schema.Types.ObjectId , ref: 'Badges' 
+    },
+    fileID: 
+        { type: mongoose.Schema.Types.ObjectId , ref: 'Files' 
+    }    
+    
+});
+
+module.exports = mongoose.model( 'Project', ProjectSchema );
---
+++ server/routes/projects.js
@@ -0,0 +1,66 @@
+const { request } = require('express');
+const express = require('express');
+const router = express.Router();
+
+//*Models
+const Project = require('../models/Project');
+
+// RETURN ALL CONTENTS ON A CERTAIN PROJECT
+//TODO: GET projects/:id
+router.get('/:projectsId', (request, response) => {
+    Project.findOne(
+        { _id: request.params.projectsId },
+        )
+    .then( (result) => {
+        console.log( result );
+        if( typeof result === 'object' ){
+            response.status(202).send( result );
+        }
+    });
+});
+
+// CREATE A NEW PROJECT
+// Parameter ID is the author of the project
+//TODO: GET projects/:id
+router.project('/:id', ( request, response ) => {
+    let newProject = new Project({
+        ...request.body,
+        author: request.params.id
+    });
+    newProject.save().then( result => {
+        response.send({ status: "New project has been created" });
+    });
+});
+
+
+
+//UPDATE A PROJECT
+//TODOl: PATCH projects/:id
+router.put('/:id', ( request, response ) => {
+    const projectId = request.params.id;
+    Project.updateOne(
+        { _id: projectId }, 
+        { $set: { ...request.body } })
+    .then( result => {
+        if( result.modifiedCount === 1 ){
+            response.send({ status: "Project has been updated" });
+        }
+    });
+});
+
+
+// DELETE A PROJECT
+//TODO: GET projects/:id
+router.delete('/:id', ( request, response ) => {
+    Project.deleteOne({ _id: request.params.id })
+    .then( result => {
+        if( result.deletedCount === 1 ){
+            response.send({
+                status: "Project has been deleted"
+            });
+        }
+    });
+});
+
+
+module.exports = router;
