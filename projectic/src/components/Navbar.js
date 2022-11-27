b85abc1524441b8ea2fdfc4e3f26cb5f942a90e4
Edreyel Adora <edreyel.aadora@gmail.com>
2022-11-27 15:58:08 +0800

    bugfix: fix spacing issues

--- projectic/src/components/Navbar.js
+++ projectic/src/components/Navbar.js
@@ -2,22 +2,24 @@ import nav_logo from "../assets/logo/nav-logo.png"
 
 const Navbar = () => {
     return (
-        <header class="container">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-                {/*Left Logo */}
-                <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                    <img alt="navbar-logo" src={nav_logo} width="200" />
-                </a>
-                {/* Middle Navigation */}
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#?" class="nav-link px-5 link-dark">Why Projectic</a></li>
-                    <li><a href="#?" class="nav-link px-5 link-dark">Features</a></li>
-                    <li><a href="#?" class="nav-link px-5 link-dark">Contact Us</a></li>
-                </ul>
-                {/* Right Navigation */}
-                <div class="col-md-3 text-end">
-                    <button type="button" class="btn btn-outline-primary me-2">Login</button>
-                    <button type="button" class="btn btn-primary me-2">Register</button>
+        <header className="p-3">
+            <div className="container">
+                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    {/*Left Logo */}
+                    <a href="/" className="d-flex align-items-center mb-2 mb-lg-0">
+                        <img className="bi me-2" alt="navbar-logo" src={nav_logo} width="150" />
+                    </a>
+                    {/* Middle Navigation */}
+                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="#?" className="nav-link px-3 link-dark">Why Projectic</a></li>
+                        <li><a href="#?" className="nav-link px-3 link-dark">Features</a></li>
+                        <li><a href="#?" className="nav-link px-3 link-dark">Contact Us</a></li>
+                    </ul>
+                    {/* Login Navigation */}
+                    <div className="text-end">
+                        <button type="button" className="btn btn-outline-primary me-2">Login</button>
+                        <button type="button" className="btn btn-primary me-2">Register</button>
+                    </div>
                 </div>
             </div>
         </header>
