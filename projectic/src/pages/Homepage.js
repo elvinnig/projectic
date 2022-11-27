4afd76fdc6c25c73c1d31aa3ecf511edf36e51ff
Edreyel Adora <edreyel.aadora@gmail.com>
2022-11-27 22:56:44 +0800

    feat: add Features page

--- projectic/src/pages/Homepage.js
+++ projectic/src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import LandingPage from "../components/LandingPage";
+import Features from "../components/Features";
 
 const Homepage = () => {
     return (
         <>
         <Navbar />
         <LandingPage />
+        <Features />
         </>
     )
 };
