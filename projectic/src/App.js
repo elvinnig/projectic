fb19de50e4345fcddc8e0dbd6bf3d56660e81199
IanAgtarap <agtarapian0@gmail.com>
2022-11-27 10:25:17 +0800

    Feat: registration page initial

--- projectic/src/App.js
+++ projectic/src/App.js
@@ -3,12 +3,14 @@
 import {Route, Routes} from 'react-router'
 //*Pages
 import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path='/users/log_in' element={<LoginPage />} />
+        <Route path='/users/sign_up' element={<RegistrationPage />} />
       </Routes>
     </div>
   );
