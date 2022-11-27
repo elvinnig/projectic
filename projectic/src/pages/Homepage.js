9357fa46e8150b3bf0fa6c7bf835e8752be89258
Edreyel Adora <edreyel.aadora@gmail.com>
2022-11-27 22:16:17 +0800

    feat: add Why Projectic page

--- projectic/src/pages/Homepage.js
+++ projectic/src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import LandingPage from "../components/LandingPage";
+import WhyProjectic from "../components/WhyProjectic";
 
 const Homepage = () => {
     return (
         <>
         <Navbar />
         <LandingPage />
+        <WhyProjectic />
         </>
     )
 };
